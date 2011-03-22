003de08a1f1ec0bdddbfb600b989a4e53cc74e5c
Gergely Imreh <imrehg@gmail.com>
2011-03-22 18:31:12 +0800

    Create simple admin action as example

--- who/whosaidthat/puzzle/admin.py
+++ who/whosaidthat/puzzle/admin.py
@@ -1,8 +1,18 @@
 from puzzle.models import Category, Person, Quote
 from django.contrib import admin
 
+# Simple admin action
+def make_verified(modeladmin, request, queryset):
+    queryset.update(verified=True)
+    make_verified.short_description = "Mark selected people as verified"
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['name', 'short_name']
+    ordering = ['name']
+    actions = [make_verified]
+
 admin.site.register(Category)
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Quote)
 
 
