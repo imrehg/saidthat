ddbdabf2cac605a5653eeae6b2c71cf22312b55e
Gergely Imreh <imrehg@gmail.com>
2011-03-23 13:32:53 +0800

    api: create all fields in the category

--- who/whosaidthat/api/handlers.py
+++ who/whosaidthat/api/handlers.py
@@ -2,11 +2,12 @@ from piston.handler import BaseHandler
 from piston.utils import rc
 from puzzle.models import Category
 from django.db import IntegrityError
+from django.template.defaultfilters import slugify
 
 class CategoryHandler(BaseHandler):
    allowed_methods = ('GET', 'POST')
    model = Category
-   fields = ('name', )
+   fields = ('name', 'description', 'uri', )
 
    def read(self, request):
        """ Return all the categories """
@@ -15,7 +16,11 @@ class CategoryHandler(BaseHandler):
    def create(self, request):
        """ Add new category """
        data = request.POST
-       em = self.model(name=data['name'])
+       em = self.model(name=data['name'],
+                       slug=slugify(data['name']),
+                       description=data['description'],
+                       uri=data['uri'],
+                       )
        try:
            em.save()
            resp = rc.CREATED
